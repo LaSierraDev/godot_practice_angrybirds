51e4b6b36a512adc40c6563363f550101955a09c
José de la Sierra <lasierradev@gmail.com>
2026-02-10 12:12:08 +0100

    [FEAT] Despawn player behavior implemented

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,7 +4,7 @@ var is_dragged: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	sleeping_state_changed.connect(_on_sleeping_state_changed)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta: float) -> void:
@@ -21,5 +21,4 @@ func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> voi
 		self.freeze = false
 
 func _on_sleeping_state_changed() -> void:
-	if self.sleeping:
-		queue_free()
+	self.queue_free()
