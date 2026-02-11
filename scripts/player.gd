53faeeef10218dd96c04710fee7b56a6863d4627
José de la Sierra <lasierradev@gmail.com>
2026-02-11 12:31:07 +0100

    [FEAT] The player's impulse preview line, implemented

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,12 @@
 extends RigidBody2D
 
+@onready var line_2d: Line2D = $Line2D
+
 @export var force_impulse: float = 10.0
 @export var max_drag_distances: float = 150.0
 
 var is_dragged: bool = false
+var is_moved: bool = false
 var start_position: Vector2
 
 func _ready() -> void:
@@ -14,11 +17,21 @@ func _ready() -> void:
 func _physics_process(_delta: float) -> void:
 	if is_dragged:
 		_drag()
+		_update_line() 
 		_check_release()
+
+func _update_line():
+	_clean_points()
+	line_2d.add_point(to_local(start_position))
+	line_2d.add_point(to_local(self.position))
+
+func _clean_points() -> void:
+	line_2d.clear_points()
 
 func _check_release():
 	if Input.is_action_just_released("drag"):
 		_impulse()
+		_clean_points()
 
 func _drag():
 	var mouse_position = get_global_mouse_position()
@@ -40,8 +53,12 @@ func _impulse():
 	apply_impulse(force_vector * force_impulse)
 
 func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
+		
 	if event.is_action_pressed("drag"):
-		is_dragged = true
+		if event.position != self.position:
+			is_dragged = true
+		else: 
+			is_dragged = false
 
 func _on_sleeping_state_changed() -> void:
 	self.queue_free()
