c5cb6ea63ae994c1789ac16a9d4fe53f34fbaa3e
José de la Sierra <lasierradev@gmail.com>
2026-02-11 14:47:59 +0100

    [FEAT] Enemy kill condition implemented

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,5 +1,6 @@
 extends RigidBody2D
 
+@export var velocity_treshold: float = 50.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,6 +10,16 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
+func destroy_me() -> void:
+	queue_free()
+
 func _on_body_entered(body: Node) -> void:
 	if body.is_in_group("player"):
-		queue_free()
+		destroy_me()
+		return
+	
+	if body is RigidBody2D and body.linear_velocity.length() > velocity_treshold:
+		print("Body Linear_velocity sin normalizar: " + str(body.linear_velocity))
+		print("Body Linear_velocity normalizada: " + str(body.linear_velocity.normalized()))
+		if body.linear_velocity.normalized().y > 0:
+			destroy_me()
